28f1d33054dd81761f21437097d997e93edc9a55
Jude Augustine Job <judeaugustinej@gmail.com>
2015-05-15 12:32:39 +0530

    Create new.py

---
+++ Magic-Methods/new.py
@@ -0,0 +1,12 @@
+
+"""
+__new__() takes the class as the first argument.
+It is used to create the object.
+"""
+class New(object):
+
+    def __new__(cls,*args):
+
+        print(cls,*args)
+
+objNew = New(5,6)  #<class '__main__.New'> 5 6
