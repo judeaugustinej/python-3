bf89311122ead5b08046c14356392049e061dcdd
Jude Augustine Job <judeaugustinej@gmail.com>
2015-05-29 18:14:44 +0530

    Create future.py

---
+++ future.py
@@ -0,0 +1,9 @@
+"""
+A future statement is a directive to the compiler that a particular module should be compiled 
+using syntax or semantics that will be available in a specified future release of Python.
+Intended to ease migration to future versions of Python  
+"""
+
+from __future__ import print_function
+
+print('# of entries', len(dictionary), file=sys.stderr)
