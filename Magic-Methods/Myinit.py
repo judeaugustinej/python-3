34cd4c35d50f620c94f42ae90a22095fbadaeb3c
Jude Augustine Job <judeaugustinej@gmail.com>
2015-06-05 16:35:01 +0530

    Update Myinit.py

--- Magic-Methods/Myinit.py
+++ Magic-Methods/Myinit.py
@@ -2,6 +2,16 @@
 __init__() takes object as the first argument.
 It is uset to initailze the object.
 """
+class Foo(object):
+    
+    def __init__(self):
+        print(self)
+        
+a = Foo()
+#<__main__.Foo object at 0x02C4E8D0>
+print(a)
+#<__main__.Foo object at 0x02C4E8D0>
+
 
 class Init(object):
 
@@ -10,3 +20,6 @@ class Init(object):
         print(self,*args)
 
 objNew = Init(5,6)    #<__main__.Init object at 0x02E5E970> 5 6
+
+
+
