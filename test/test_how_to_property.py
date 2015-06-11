9debaebf3d9beac513931492dc34b7af3eb2e596
Jude Augustine Job <judeaugustinej@gmail.com>
2015-06-11 19:33:45 +0530

    incomplete

--- test/test_how_to_property.py
+++ test/test_how_to_property.py
@@ -1,1 +1,12 @@
+import unittest
 
+import how_to_property
+
+class PersonTestCase(unittest.TestCase):
+  
+  def setUp(self):
+    self.personObj = how_to_property.Person("Jude",25)
+    
+  def test_delete_age(self):
+    self.assertRaises(AttributeError,del self.personObj.name)
+    
