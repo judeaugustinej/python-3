f57864cdfe80f3b925d0729edb7010cd964e96e3
Jude Augustine Job <judeaugustinej@gmail.com>
2015-08-26 14:57:23 +0530

    Create basicContextManager.py

---
+++ ContextManager/basicContextManager.py
@@ -0,0 +1,25 @@
+class MyContext:
+
+    def __init__(self):
+        print("__init__")
+
+    def __enter__(self):
+        print("In the context__enter__")
+
+    def __exit__(self,exc_type,exc_val,exc_tb):
+        print("__exit__()")
+
+if __name__ == "__main__":
+    with MyContext():
+        print("What is context manager")
+
+# Output
+"""
+[sunkaray@SUT-LNX-1 contextManger]$ python3 basicContext.py
+__init__
+In the context__enter__
+What is context manager
+__exit__()
+[sunkaray@SUT-LNX-1 contextManger]$
+
+"""
